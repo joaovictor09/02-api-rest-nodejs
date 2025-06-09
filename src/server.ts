d06a7259eb816142d9069e83e5aea50acd3f9b3b
João Victor <joaovictordasilva0911@gmail.com>
2025-06-08 22:45:29 -0300

    initial structure

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on http://localhost:3000')
+})
